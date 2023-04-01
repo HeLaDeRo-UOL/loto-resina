c3f147ded0fcd53b2640e5a2a9b36e7bc911d343
Martin <pulgarodriguez99@gmail.com>
2023-04-02 01:58:13 +0200

    retoques

--- src/App.js
+++ src/App.js
@@ -22,6 +22,7 @@ function App() {
         <Route path="*" element={<h1>Error : 404 "not found"</h1>} />
         <Route path="/Formulario" element = {<Form/>}/>
       </Routes>
+      <Footer/>
     </BrowserRouter>
   );
 }
